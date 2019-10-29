bdf8d48b7a8713665a23523f583fac32a5e24fc3
Kevin Keaveney <kevinkeaveney@MacBook-Pro.local>
2019-10-29 22:59:42 +0000

    address tests

---
+++ test/votingSystemTest.js
@@ -0,0 +1,16 @@
+contract('Flight Surety Tests', async (accounts) => {
+
+    let owner = accounts[0];
+    let airline2 = accounts[1];
+    let airline3 = accounts[2];
+    let airline4 = accounts[3];
+    let airline5 = accounts[4];
+    let airline6 = accounts[5];
+
+    console.log(owner);
+    console.log(airline2);
+    console.log(airline3);
+    console.log(airline4);
+    console.log(airline5);
+    console.log(airline6);
+  });
