fe83020a82ed35ad58019abb196bd240c37059f2
Kevin Keaveney <kevinkeaveney@MacBook-Pro.local>
2019-10-29 21:30:08 +0000

    First commit

---
+++ contracts/ElectionContract.sol
@@ -0,0 +1,46 @@
+pragma solidity ^0.4.24;
+
+contract ElectionContract {
+
+    struct Candidate {
+        string name;
+        string party;
+    }
+
+    struct Voter {
+        string name;
+        uint age;
+    }
+
+    struct Election {
+        uint256 registrationPeriod;
+        uint256 votingPeriod;
+        uint256 endTime;
+    }
+
+    Candidate[] public candidates;
+    uint public numberOfCandidates;
+
+    Voter[] public voters;
+    uint public numberOfVoters;
+
+    Election public election;
+
+    function _registerCandidate(string _name, string _party) public {
+        candidates.push(Candidate(_name, _party));
+        numberOfCandidates++;
+    }
+
+    function _registerVoter(string _name, uint _age) public {
+        voters.push(Voter(_name,_age));
+        numberOfVoters++;
+    }
+
+    function initializeElection(uint256 _registrationPeriod, uint256 _votingPeriod, uint256 _endTime) public {
+        election.registrationPeriod = _registrationPeriod;
+        election.votingPeriod = _votingPeriod;
+        election.endTime = _endTime;
+    }
+
+
+}
