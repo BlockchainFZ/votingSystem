73556a7db49e192d9f7d236066c66e25c8eb1627
Kevin Keaveney <kevinkeaveney@MacBook-Pro.local>
2019-10-31 22:33:02 +0000

    Add candidate test

--- test/votingSystemTest.js
+++ test/votingSystemTest.js
@@ -3,20 +3,28 @@ var ElectionContract = artifacts.require("ElectionContract");
 contract('Flight Surety Tests', async (accounts) => {
 
     let owner = accounts[0];
-    let airline2 = accounts[1];
-    let airline3 = accounts[2];
-    let airline4 = accounts[3];
-    let airline5 = accounts[4];
-    let airline6 = accounts[5];
+
 
     console.log(owner);
-    console.log(airline2);
-    console.log(airline3);
-    console.log(airline4);
-    console.log(airline5);
-    console.log(airline6);
+
 
     it(`returns contract owner`, async () => {
       let data = await ElectionContract.deployed();
-    })
+      let status = await data.isOwner.call();
+
+      assert.equal(status, true, "Not the contract owner");
+
+    });
+
+    it(`Allows any address to add a candidate`, async () => {
+      let data = await ElectionContract.deployed();
+      let numberOfCandidates = await data.numberOfCandidates.call();
+      assert.equal(numberOfCandidates, 0, "initial contract has 0 candidates");
+
+      await data._registerCandidate("Boris Johnson", "Tory");
+      numberOfCandidates = await data.numberOfCandidates.call();
+      assert.equal(numberOfCandidates, 1, "Contract has 1 candidate");
+      console.log('numberOfCandidates',numberOfCandidates.toNumber());
+
+    });
   });
