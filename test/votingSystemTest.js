0dd39214a903166d3363459d442eef7b6e75a57a
Admin <admin@Admins-MacBook-Pro-2.local>
2019-11-01 14:12:09 +0000

    RegisterVoter onlyOwner tests

--- test/votingSystemTest.js
+++ test/votingSystemTest.js
@@ -15,15 +15,40 @@ contract('Flight Surety Tests', async (accounts) => {
 
     });
 
-    it(`Allows any address to add a candidate`, async () => {
+    it(`Allows contract owner to register a candidate`, async () => {
       let data = await ElectionContract.deployed();
       let numberOfCandidates = await data.numberOfCandidates.call();
       assert.equal(numberOfCandidates, 0, "initial contract has 0 candidates");
 
-      await data._registerCandidate("Boris Johnson", "Tory");
+      await data._registerCandidate("Boris Johnson", "Tory", {from:owner});
       numberOfCandidates = await data.numberOfCandidates.call();
       assert.equal(numberOfCandidates, 1, "Contract has 1 candidate");
 
+        try {
+          await data._registerCandidate("Boris Johnson", "Tory", {from:account2});
+        } catch (error) {
+          console.log("Only Contract Owner can register candidate");
+        }
 
-    });
+      });
+
+    it(`Allows contract owner to register voter`, async () => {
+      let data = await ElectionContract.deployed();
+      let numberOfVoters = await data.numberOfVoters.call();
+      assert.equal(numberOfVoters, 0, "Contract initialised with no voters");
+
+      await data._registerVoter("John B", 25, {from:owner});
+      numberOfVoters = await data.numberOfVoters.call();
+      assert.equal(numberOfVoters, 1, "One voter has been registered");
+
+        try {
+          await data._registerVoter("Kenny Ken", 40, {from:account2});
+        } catch (error) {
+          console.log("Only Contract Owner can register voters");
+        }
+
+      });
+
+
+
   });
