fb09a04b6d4c8288a63ad5b5eb6a831ff132dea8
Kevin Keaveney <kevinkeaveney@MacBook-Pro.local>
2019-11-02 17:11:42 +0000

    _remainingRegistrationPeriod test

--- test/votingSystemTest.js
+++ test/votingSystemTest.js
@@ -6,7 +6,7 @@ contract('Flight Surety Tests', async (accounts) => {
     let account2 = accounts[1];
 
 
-    it(`confirms contract owner`, async () => {
+    it(`Confirms contract owner`, async () => {
       let data = await ElectionContract.deployed();
       let status = await data._isOwner(owner);
       assert.equal(status, true, "Not the contract owner");
@@ -15,46 +15,20 @@ contract('Flight Surety Tests', async (accounts) => {
 
     });
 
-    it(`Allows contract owner to register a candidate`, async () => {
+    it(`Allows any address to add a candidate`, async () => {
       let data = await ElectionContract.deployed();
       let numberOfCandidates = await data.numberOfCandidates.call();
       assert.equal(numberOfCandidates, 0, "initial contract has 0 candidates");
 
-      await data._registerCandidate("Boris Johnson", "Tory", {from:owner});
+      await data._registerCandidate("Boris Johnson", "Tory");
       numberOfCandidates = await data.numberOfCandidates.call();
       assert.equal(numberOfCandidates, 1, "Contract has 1 candidate");
-
-        try {
-          await data._registerCandidate("Boris Johnson", "Tory", {from:account2});
-        } catch (error) {
-          console.log("Only Contract Owner can register candidate");
-        }
-
-      });
-
-    it(`Allows contract owner to register voter`, async () => {
-      let data = await ElectionContract.deployed();
-      let numberOfVoters = await data.numberOfVoters.call();
-      assert.equal(numberOfVoters, 0, "Contract initialised with no voters");
-
-      await data._registerVoter("John B", 25, {from:owner});
-      numberOfVoters = await data.numberOfVoters.call();
-      assert.equal(numberOfVoters, 1, "One voter has been registered");
-
-        try {
-          await data._registerVoter("Kenny Ken", 40, {from:account2});
-        } catch (error) {
-          console.log("Only Contract Owner can register voters");
-        }
-
-      });
-
-    it(`returns remaining registration period`, async () => {
-      let data = await ElectionContract.deployed();
-      let remainingRegPeriod = await data._remainingRegistrationPeriod.call();
-      console.log("remainingRegPeriod ",remainingRegPeriod.toNumber());
     });
 
+    it('Returns remainingRegistrationPeriod',async () => {
+      let data = await ElectionContract.deployed();
+      let remainingRegistrationPeriod = await data._remainingRegistrationPeriod.call();
+      console.log(remainingRegistrationPeriod.toNumber());
 
-
+    });
   });
