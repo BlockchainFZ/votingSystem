491944b366288bd5f51232726273d2ec78dec2d8
Admin <admin@Admins-MacBook-Pro-2.local>
2019-11-04 13:05:22 +0000

    Recompiled Contracts

--- contracts/ElectionContract.sol
+++ contracts/ElectionContract.sol
@@ -27,58 +27,88 @@ contract ElectionContract is Ownable {
         bool closeVotingPeriod;
         bool closeElectionPeriod;
     }
-    Election public election;
 
-    Candidate[] private candidates;
+    Election public election;
+    //Voter public voter;
+    //Candidate public candidate;
+    mapping(address => bool) isCandidateValid;
+    mapping(address => bool) isVoterValid;
+
+
+    Candidate[] public candidates;
     uint public numberOfCandidates;
 
-    Voter[] private voters;
-    uint private numberOfVoters;
+
+    Voter[] public voters;
+    uint public numberOfVoters;
+
 
     constructor() public {
      election = Election(now, 1 days, 2 days, 2 days, false, false, false);
 
     }
 
-    modifier isvalidRegistrationPeriod () {
-        require(_remainingRegistrationPeriod() <= election.registrationPeriod);
-     _;
-    }
+    // modifiers
 
-    modifier voteIsOpen()  {
-        require(now < election.electionTimeStamp.add(election.votingPeriod));
+    modifier registrationPeriodIsOpen()  {
+        require(election.closeRegistrationPeriod == false);
     _;
     }
 
-    modifier isvalidVotingPeriod() {
-        require(_remainingVotingPeriod() <= election.votingPeriod);
+    modifier votingPeriodIsOpen() {
+      require(election.closeVotingPeriod == false);
+      _;
+    }
+
+    modifier closeElectionPeriod() {
+      require(election.closeElectionPeriod == false);
+      _;
+    }
+
+    modifier validCandidate(address _address) {
+      require(isCandidateValid[_address] == true);
+      _;
+    }
+
+    modifier validVoter(address _address) {
+        require(isVoterValid[_address] == true);
         _;
     }
+
+    // validate candidates
+
+    function validateCandidate(address _address) public returns (bool) {
+        return isCandidateValid[_address] = true;
+    }
+
+    function validateVoter(address _address) public returns (bool) {
+        return isVoterValid[_address] = true;
+    }
+
 
     function _isOwner(address _address) public view returns(bool) {
       return _address == owner();
     }
 
-    function _registerCandidate(string memory _name, string memory _party) public isvalidRegistrationPeriod {
+    // Open Registration Functions
+
+    function _registerCandidate(string memory _name, string memory _party) public registrationPeriodIsOpen validCandidate(msg.sender){
         candidates.push(Candidate(_name, _party));
         numberOfCandidates++;
     }
 
-    function _registerVoter(string memory _name, uint _age) public onlyOwner voteIsOpen {
+    function _registerVoter(string memory _name, uint _age) public onlyOwner registrationPeriodIsOpen validVoter(msg.sender) {
         voters.push(Voter(_name,_age));
         numberOfVoters++;
     }
 
-    function _remainingRegistrationPeriod() public view returns (uint256){
-        return now.sub(election.electionTimeStamp);
 
-    }
+    // voting Functions
 
-    function _remainingVotingPeriod() public view returns (uint256) {
-        return now.sub(election.electionTimeStamp);
-    }
 
-    function _closeRegistrationPeriod() public returns (bool) {
+    // Close Registration Functions
+
+ /*   function _closeRegistrationPeriod() public returns (bool) {
             return election.closeRegistrationPeriod = true;
     }
 
@@ -87,9 +117,8 @@ contract ElectionContract is Ownable {
     }
 
     function _closeElection() public returns (bool) {
-            return election.closeElectionPeriod = false;
+            return election.closeElectionPeriod = true;
     }
+    */
 
-
-
-  }
+}
