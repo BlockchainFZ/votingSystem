53592c6b2d201b7f83be535d40d5bbd386f073b6
Admin <admin@Admins-MacBook-Pro-2.local>
2019-12-02 13:05:44 +0000

    Allows a registered voter, to vote'

--- test/truffleAssertions.js
+++ test/truffleAssertions.js
@@ -9,6 +9,7 @@ contract('Truffle Assertion Tests', async (accounts) => {
     let contract;
     let owner = accounts[0];
     let account2 = accounts[1];
+    let account3 = accounts[2];
 
     beforeEach(async () => {
       contract = await ElectionContract.new({ from:owner});
@@ -50,24 +51,35 @@ contract('Truffle Assertion Tests', async (accounts) => {
 
     it(`Demonstrates an invalid Candidate`, async() => {
         // Attempt to retrieve candidate without opening an election//
-        await truffleAssert.reverts(contract.getCandidate(owner), "Candidate is invalid");
+        await truffleAssert.reverts(contract.getCandidate(owner), "Candidate is not registered");
     });
 
     it(`Demonstrates a Candidate is invalid`, async() =>{
         // Open an election, register a candidate, attempt to retrieve non registered candidate//
         await contract.setElectionAccess(true);
         await contract._registerCandidate(owner,"Boris Johnson", "Tory");
-        await truffleAssert.reverts(contract.getCandidate(account2), "Candidate is invalid");
+        await truffleAssert.reverts(contract.getCandidate(account2), "Candidate is not registered");
     });
 
 
-    it(`Demonstrates an invalid vote`, async() => {
+    it(`Demonstrates an unregistered Voter cannot vote`, async() => {
+      // Open an election, Open Voting, cast vote
       await contract.setElectionAccess(true);
       await contract.setVotingAccess(true);
-      await contract.vote(true);
-      await truffleAssert.reverts(contract.vote("Con"), "Vote is not valid");
+      await truffleAssert.reverts(contract.vote("Con"), "Voter is not registered");
 
     });
+
+    it('Demostrates a voter only vote once', async() => {
+      // Open an election, Open Voting, cast vote, attempt to vote twice
+      await contract.setElectionAccess(true);
+      await contract.setVotingAccess(true);
+      await contract._registerVoter(owner, "Dave McBade", 45);
+      await contract.vote("Con");
+      await truffleAssert.reverts(contract.vote("Lab"), "Voter is either unregistered, or has already voted");
+    });
+
+
 
 
 
